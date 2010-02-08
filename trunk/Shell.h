3b30c0c5ac7c2b4e59febc3d83bc1d98c1b8eb06
jtxuee@gmail.com <jtxuee@gmail.com@716a2f10-c84c-11dd-bf7c-81814f527a11>
2010-02-08 03:07:15 +0000

    增加shell文件 获取系统默认imagelist

---
+++ trunk/Shell.h
@@ -0,0 +1,12 @@
+
+#ifndef HWX_SHELL_H
+#define HWX_SHELL_H
+#include "XTypeEx.h"
+
+#ifndef WINCE
+DLLXEXPORT HIMAGELIST WINAPI GetSystemImageList(UINT uFlags);
+#endif
+
+	
+
+#endif //HWX_WINDOWEX_H
